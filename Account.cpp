b343f4b91c02e24798d8a67792e3110f733c01f5
KJ <klee687@uwo.ca>
2018-12-27 18:51:06 -0500

    Add files via upload

--- Account.cpp
+++ Account.cpp
@@ -1,9 +1,38 @@
 #include "Account.hpp"
 
-
-
 //Account has a constructor
-Account::Account(std::string name) : name(name) {}
+Account::Account(std::string name, std::string chequing, std::string saving, std::string date) : name(name), chequing(chequing), saving(saving), date(date) {}
+
+std::string Account::getName(){
+	return this->name;
+}
+
+std::string Account::getChequing(){
+	return this->chequing;
+}
+
+std::string Account::getSaving(){
+	return this->saving;
+}
+
+std::string Account::getDate(){
+	return this->date;
+}
 
 
 
+void Account::setName(std::string name){
+	this->name = name;
+}
+
+void Account::setChequing(std::string chequing){
+	this->chequing = chequing;
+}
+
+void Account::setSaving(std::string saving){
+	this->saving = saving;
+}
+
+void Account::setDate(std::string date){
+	this->date = date;
+}
--- Account.hpp
+++ Account.hpp
@@ -1,28 +1,37 @@
+/*
+	
+
+ifndef, define are called #include guards.
+
+Once the header is included, it checks if a unique value (in this case ACCOUNT_H) is defined. Then if it's not defined, it defines it and continues to the rest of the page.
+
+When the code is included again, the first ifndef fails, resulting in a blank file.
+
+That prevents double declaration of any identifiers such as types, enums and static variables.
+
+*/
 
 #ifndef ACCOUNT_H_
 #define ACCOUNT_H_
-
-
-
 #include <string>
 
+
 class Account{
-
-
-
-	//the account probably has a name??
 	std::string name;
-	
-		
-
+	std::string chequing;
+	std::string saving;
+	std::string date;
 
 public:
-	Account(std::string name);
-
-
+	Account(std::string name, std::string chequing, std::string saving, std::string date);
+	std::string getName();
+	std::string getChequing();
+	std::string getSaving();
+	std::string getDate();
+	
+	void setName(std::string name);
+	void setChequing(std::string chequing);
+	void setSaving(std::string saving);
+	void setDate(std::string date);
 };
-
-
-
-
 #endif
